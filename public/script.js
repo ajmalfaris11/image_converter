6c4722a38303e57425bbe237fe8c7fd9407f3c9e
ajmal faris <ajmalfaris.me@gmail.com>
2024-03-12 10:00:00 +0000

    Update public/script.js (line 5)

--- public/script.js
+++ public/script.js
@@ -2,3 +2,4 @@ document.addEventListener('DOMContentLoaded', function () {
     // const selectImage = document.querySelector('.select-image');
     const inputFile = document.querySelector('#file');
     const imgArea = document.querySelector('.img-area');
+
