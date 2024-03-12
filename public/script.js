2ed177c7cff54af6a612dbfc1022d1c3f51941bc
ajmal faris <ajmalfaris.me@gmail.com>
2024-03-12 10:00:00 +0000

    Update public/script.js (line 20)

--- public/script.js
+++ public/script.js
@@ -17,3 +17,4 @@ document.addEventListener('DOMContentLoaded', function () {
                 allImg.forEach(item => item.remove());
                 const imgUrl = reader.result;
                 const img = document.createElement('img');
+                img.src = imgUrl;
