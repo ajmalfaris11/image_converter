0e8d29af1d00523e316e58dd445677e7125037a5
ajmal faris <ajmalfaris.me@gmail.com>
2024-04-20 10:00:00 +0000

    Update app/layout.jsx (line 14)

--- app/layout.jsx
+++ app/layout.jsx
@@ -11,3 +11,4 @@ export default function RootLayout({ children }) {
       <head>
         <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
         <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet' />
+      </head>
