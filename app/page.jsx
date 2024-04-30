4efcf2d8f7ba38106f995690ecb8ff4784bf8e19
ajmal faris <ajmalfaris.me@gmail.com>
2024-04-30 10:00:00 +0000

    Update app/page.jsx (line 143)

--- app/page.jsx
+++ app/page.jsx
@@ -140,3 +140,4 @@ export default function Home() {
                 
                 <div 
                   className={`dropzone ${isDragActive ? 'active' : ''}`} 
+                  id="dropzone"
