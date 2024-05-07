3c14d18984f041eb81ebf90c9cde31538c1f1bd7
ajmal faris <ajmalfaris.me@gmail.com>
2024-05-07 10:00:00 +0000

    Update app/api/image/route.js (line 41)

--- app/api/image/route.js
+++ app/api/image/route.js
@@ -38,3 +38,4 @@ export async function POST(request) {
                 break
             case 'jpeg':
             case 'jpg':
+                transform = transform.jpeg({ quality: parsedQuality })
