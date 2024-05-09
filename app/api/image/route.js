2ad2f87dc6409bedfd8b2e64805297fdd866ce7a
ajmal faris <ajmalfaris.me@gmail.com>
2024-05-09 10:00:00 +0000

    Update app/api/image/route.js (line 67)

--- app/api/image/route.js
+++ app/api/image/route.js
@@ -64,3 +64,4 @@ export async function POST(request) {
         let contentType = `image/${ext}`
         if (ext === 'jpg') contentType = 'image/jpeg'
 
+        return new NextResponse(convertedImage, {
