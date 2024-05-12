ab79f6eae72dcfa81bfc933be47570f8b33e62c0
ajmal faris <ajmalfaris.me@gmail.com>
2024-05-12 12:02:00 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Providers } from './providers';
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -12,13 +13,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
         <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
         <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet" />
+        <script src="https://cdn.jsdelivr.net/npm/exifr/dist/full.umd.js" defer></script>
       </head>
       <body suppressHydrationWarning className="font-sans antialiased text-brand-text bg-gradient-to-br from-brand-bgstart to-brand-bgend min-h-screen overflow-hidden">
-        {children}
+        <Providers>
+          {children}
+        </Providers>
       </body>
     </html>
   );
